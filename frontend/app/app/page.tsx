53a7cdaf303ce169e5097e5a2e92446725d38b54
chobi <chobirity0228@gmail.com>
2024-04-22 19:35:58 +0900

    front/scss&axios

--- frontend/app/app/page.tsx
+++ frontend/app/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import styles from "./styles/page.module.scss";
+import styles from "./assets/styles/page.module.scss";
 
 export default function Home() {
   return (
