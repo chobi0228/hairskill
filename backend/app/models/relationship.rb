03289ff965a165033f5dc06c8e304fde585728ff
chobi <chobirity0228@gmail.com>
2024-04-22 20:04:12 +0900

    コントローラ作成

--- backend/app/models/relationship.rb
+++ backend/app/models/relationship.rb
@@ -1,2 +1,6 @@
-class Relationship < ApplicationRecord
-end
+class User < ApplicationRecord
+    has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id"
+    has_many :passive_relationships, class_name: "Relationship", foreign_key: "follow_id"
+    has_many :follows, through: :active_relationships, source: :follow
+    has_many :followers, through: :passive_relationships, source: :follower
+  end
