9476060ca9a48ef7b47391160b12ea5bf130da0f
chobi <chobirity0228@gmail.com>
2024-04-23 23:22:01 +0900

    backend/モジュールのベース作成など

--- backend/app/controllers/api/v1/contacts_controller.rb
+++ backend/app/controllers/api/v1/contacts_controller.rb
@@ -1,22 +1,29 @@
-class Api::V1::ContactsController < ApplicationController
-  def index
-  end
+class Api::V1::ContactsController < Api::V1::ApplicationController
+  extend ManualProcess
 
-  def new
+  def index
+    manual_process(1, Contact)
   end
 
   def show
+    manual_process(2, Contact)
   end
 
   def edit
+    manual_process(2, Contact)
   end
 
   def create
+    manual_process(3, Contact, )
   end
 
   def update
+    manual_process(4, Contact)
   end
 
   def destroy
+    manual_process(5, Contact)
   end
+  private
+  
 end
--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,10 @@
 class Api::V1::UsersController < ApplicationController
+  def sign_up
+  end
+
+  def login
+  end
+  
   def index
   end
 
