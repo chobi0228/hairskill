0a98a86d845f80cb759a25fec85f44a1207f1d00
chobi <chobirity0228@gmail.com>
2024-05-01 21:31:22 +0900

    front/ページ遷移

--- frontend/app/src/pages/all/components/header.component.tsx
+++ frontend/app/src/pages/all/components/header.component.tsx
@@ -1,8 +1,9 @@
+import Link from "next/link";
 import {urls, generalWordSettings} from "../../../constants/general";
 export default function Header() {
     return (
       <>
-        <header>{generalWordSettings.aplicaton_title}</header>
+        <header><Link href={"/"} className="header">{generalWordSettings.aplicaton_title}</Link></header>
       </>
     );
 }
