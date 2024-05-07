8ba47e0ee62c35c55fa2db42f5e1d5844818f84d
chobi <chobirity0228@gmail.com>
2024-05-07 20:31:32 +0900

    ヘッダー

--- frontend/app/src/pages/all/components/header.component.tsx
+++ frontend/app/src/pages/all/components/header.component.tsx
@@ -3,7 +3,7 @@ import {urls, general_word_setting} from "../../../constants/general";
 export default function Header() {
     return (
       <>
-        <header className="header">{general_word_setting.aplicaton_title}</header>
+        <header>{general_word_setting.aplicaton_title}</header>
       </>
     );
 }
