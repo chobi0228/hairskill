d1faeace59b2db5df746131a5017e94fb16e98e4
chobi <chobirity0228@gmail.com>
2024-05-08 23:37:34 +0900

    共通部分

---
+++ frontend/app/src/pages/contacts/index.tsx
@@ -0,0 +1,5 @@
+import { urls } from "../../constants/general";
+import { getSaveData } from "../../constants/local_storage";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
